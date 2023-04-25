bd9314e035a58351363f1fce56d8254682235167
KennethFlye <10434642@ucn.dk>
2023-04-25 12:19:35 +0200

    Model lag opsat

--- SemesterProjekt3Api/Model/Booking.cs
+++ SemesterProjekt3Api/Model/Booking.cs
@@ -2,5 +2,12 @@
 {
     public class Booking
     {
+
+        public int BookingId { get; set; }
+        public DateTime TimeOfPurchase { get; set; }
+        public double Total { get; set; }
+        public String CustomerPhone { get; set; }
+        public List<Seat> BookedSeats { get; set; }
+
     }
 }
