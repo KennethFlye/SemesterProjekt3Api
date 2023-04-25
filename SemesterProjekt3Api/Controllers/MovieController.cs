1f3d0585674ef1d2bb1e03e3a07a498ddec82f02
bjoernUCN <10434634@ucn.dk>
2023-04-26 01:13:21 +0200

    Create MovieController.cs

---
+++ SemesterProjekt3Api/Controllers/MovieController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SemesterProjekt3Api.Database;
+
+namespace SemesterProjekt3Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private readonly ILogger<MovieController> _logger;
+
+        public MovieController(ILogger<MovieController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{tst}")] //Hurtig test
+        public ActionResult<string> Get(string tst)
+        {
+            DBConnection dbc = DBConnection.GetInstance();
+            dbc.OpenConnection();
+            string returnString = dbc.GetConnection().State.ToString() + " " + dbc.GetConnection().Database.ToString();
+            dbc.CloseConnection();
+
+            return Ok($"recieved: {tst} {returnString}");
+        }
+
+
+    }
+
+
+
+}
