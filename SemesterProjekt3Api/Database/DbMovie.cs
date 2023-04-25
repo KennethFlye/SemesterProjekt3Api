d6d3e49b9d5c6f1821c35d2bb52e79e6af4349d7
KennethFlye <10434642@ucn.dk>
2023-04-25 13:48:50 +0200

    Db Movies begyndt at blive implementeret

--- SemesterProjekt3Api/Database/DbMovie.cs
+++ SemesterProjekt3Api/Database/DbMovie.cs
@@ -1,11 +1,34 @@
-﻿using SemesterProjekt3Api.Model;
+﻿using Dapper;
+using Microsoft.Win32.SafeHandles;
+using SemesterProjekt3Api.Model;
+using System.Data.SqlClient;
 
 namespace SemesterProjekt3Api.Database
 {
     public class DbMovie
     {
+        private string _getMovieInfoQuery = "SELECT infoId, title, length, genre, pgRating, premiereDate FROM MovieInfo";
+        private string _getMovieCopyQuery = "SELECT copyId, language, is3D, price, movieinfoId FROM MovieCopy";
+        private string _getMovieCopiesQuery = "SELECT * FROM MovieCopy mc INNER JOIN MovieInfo mi ON mc.movieinfoId = mi.infoId";
+
         internal List<MovieCopy> getMovies()
         {
+            List<MovieCopy> foundCopies = new List<MovieCopy>();
+            List<MovieInfo> foundInfos = new List<MovieInfo>();
+
+            DBConnection dbConnection = DBConnection.GetInstance();
+            SqlConnection connection = dbConnection.GetConnection();
+
+            foundInfos = connection.Query<MovieInfo>(_getMovieInfoQuery).ToList();
+
+            foundCopies = connection.Query<MovieCopy>(_getMovieCopyQuery, (MovieCopy, foundInfos) =>
+            {
+                for(int i = 0, i < foundInfos.Count; i++)
+                {
+
+                }
+            }).ToList();
+
             throw new NotImplementedException();
         }
 
