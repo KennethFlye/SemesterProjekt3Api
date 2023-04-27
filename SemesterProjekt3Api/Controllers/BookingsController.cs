e7f244dc0828d9a0fe66e366eb6b410796708c98
bjoernUCN <103989859+bjoernUCN@users.noreply.github.com>
2023-04-27 13:47:21 +0200

    post melon

--- SemesterProjekt3Api/Controllers/BookingsController.cs
+++ SemesterProjekt3Api/Controllers/BookingsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SemesterProjekt3Api.Database;
 using SemesterProjekt3Api.Model;
 using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace SemesterProjekt3Api.Controllers
 {
@@ -39,9 +40,9 @@ namespace SemesterProjekt3Api.Controllers
         }
 
         [HttpPost]
+        [Route("")]
         public ActionResult Post([FromBody] Booking newBooking)
         {
-
             // validate and save to database
             bool badThingsHappened = false;
             if (badThingsHappened)
@@ -50,6 +51,33 @@ namespace SemesterProjekt3Api.Controllers
             }
             else
             {
+                /*
+                //Stub
+                Booking bookingTestJsonConvert = new Booking();
+                bookingTestJsonConvert.BookingId = 3;
+                bookingTestJsonConvert.Total = 240;
+                bookingTestJsonConvert.TimeOfPurchase = DateTime.Now;
+                bookingTestJsonConvert.CustomerPhone = "6412563";
+
+                Seat seat1 = new Seat();
+                seat1.SeatId = 1;
+                seat1.RowNumber = 1;
+                seat1.SeatNumber = 1;
+                seat1.ShowroomId = 1;
+                Seat seat2 = new Seat();
+                seat2.SeatId = 2;
+                seat2.RowNumber = 2;
+                seat2.SeatNumber = 2;
+                seat2.ShowroomId = 2;
+                bookingTestJsonConvert.BookedSeats = new List<Seat>() {seat1,seat2};
+                String stringLog = JsonSerializer.Serialize(bookingTestJsonConvert);
+
+                return Created("Uuuh test " +stringLog,newBooking);
+                //Stub
+
+                */
+
+
                 _dbBooking.AddBooking(newBooking);
 
                 return Created("", newBooking);
