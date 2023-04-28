2f30ddc1bb28039cc5cd9e86f40bd11e092e3dc7
bjoernUCN <103989859+bjoernUCN@users.noreply.github.com>
2023-04-28 09:19:57 +0200

    Completed Post

--- SemesterProjekt3Api/Database/DbBooking.cs
+++ SemesterProjekt3Api/Database/DbBooking.cs
@@ -1,8 +1,8 @@
 ﻿using Dapper;
 using Microsoft.Extensions.Primitives;
 using SemesterProjekt3Api.Model;
-using System.Data;
 using System.Data.SqlClient;
+using System.Transactions;
 
 namespace SemesterProjekt3Api.Database
 {
@@ -10,63 +10,45 @@ namespace SemesterProjekt3Api.Database
     {
         public void AddBooking(Booking newBooking)
         {
-            DBConnection dbc = DBConnection.GetInstance();
-            SqlConnection sqlConnection = dbc.GetConnection();
-
-            int idReturn = 0;
-            try
+            using (var scopeTransaction = new TransactionScope())
             {
-                string sql = "INSERT INTO [Booking] (timeOfPurchase, total, customerPhone, showingId) OUTPUT INSERTED.bookingId VALUES (@timeOfPurchase, @total, @phone, @showingId)";
-                idReturn = sqlConnection.QuerySingle<int>(
-                sql,
-                new
+                try
                 {
-                    timeOfPurchase = newBooking.TimeOfPurchase,
-                    total = newBooking.Total,
-                    phone = newBooking.CustomerPhone,
-                    showingId = newBooking.Showing.showingId
-                });
-            }
-            catch (Exception)
-            {
+                    DBConnection dbc = DBConnection.GetInstance();
+                    SqlConnection sqlConnection = dbc.GetConnection();
+
+                    int idReturn = 0;
+                    string sql = "INSERT INTO [Booking] (timeOfPurchase, total, customerPhone, showingId) OUTPUT INSERTED.bookingId VALUES (@timeOfPurchase, @total, @phone, @sId)";
+                    idReturn = sqlConnection.QuerySingle<int>(
+                    sql,
+                    new
+                    {
+                        timeOfPurchase = newBooking.TimeOfPurchase,
+                        total = newBooking.Total,
+                        phone = newBooking.CustomerPhone,
+                        sId = newBooking.Showing.showingId
+                    });
+
+                    //Insert Seat
+                    foreach (Seat seat in newBooking.BookedSeats)
+                    {
+                        sqlConnection.Query(
+                            "INSERT INTO [BookingSeat] (bookingId, seatId) VALUES (@BookingId, @SeatId)",
+                            new
+                            {
+                                BookingId = idReturn,
+                                SeatId = seat.SeatId
+                            });
+                    }
+                    scopeTransaction.Complete();
+
+                }
+                catch (Exception)
+                {
+
+                }
 
             }
-            //Todo, seat stuff
-            
-            Console.WriteLine(idReturn);
-
-
-            //sqlConnection.Open();
-            /*try
-            {
-                SqlCommand command = sqlConnection.CreateCommand();
-                command.CommandText = "INSERT INTO Booking (timeOfPurchase, total, customerPhone, showingId) VALUES (@purchaseTime, @total @phone @showingId)";
-                SqlParameter time = new SqlParameter("@timeOfPurchase", SqlDbType.DateTime);
-                SqlParameter total = new SqlParameter("@total",SqlDbType.Float);
-                SqlParameter phone = new SqlParameter("@customerPhone",SqlDbType.Text, 50);
-                SqlParameter showingId = new SqlParameter("@showingId",SqlDbType.Int);
-
-                time.Value = newBooking.TimeOfPurchase;
-                total.Value = newBooking.Total;
-                phone.Value = newBooking.CustomerPhone;
-                showingId.Value = newBooking.Showing.Id;
-                
-                command.Parameters.Add(time);
-                command.Parameters.Add(total);
-                command.Parameters.Add(phone);
-                command.Parameters.Add(showingId);
-
-                //Execute
-                command.Prepare();
-                command.ExecuteNonQuery();
-
-
-
-            }
-            catch (Exception)
-            {
-                throw;
-            }*/
         }
 
         internal List<Seat> GetSeatsByShowingId(StringValues showingId)
