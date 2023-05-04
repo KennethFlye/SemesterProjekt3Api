ebdef0113e02194e0f9075d1f5ac85cc8a37df82
KennethFlye <10434642@ucn.dk>
2023-05-04 12:23:56 +0200

    getSeat implemented

---
+++ SemesterProjekt3Api/Controllers/SeatsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SemesterProjekt3Api.Database;
+using SemesterProjekt3Api.Model;
+
+namespace SemesterProjekt3Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeatsController : Controller
+    {
+
+        private DbSeat _dbSeat = new DbSeat();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("{seatId}")]
+        public ActionResult Get(int seatId)
+        {
+            Seat seat = _dbSeat.getSeat(seatId);
+
+            if (seat == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(seat);
+        }
+    }
+}
---
+++ SemesterProjekt3Api/Database/DbSeat.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using SemesterProjekt3Api.Model;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace SemesterProjekt3Api.Database
+{
+    public class DbSeat
+    {
+
+        private string _getSeatBySeatIdQuery = "Select seatId, rowNumber, seatNumber, showRoomId FROM Seat WHERE seatId = @seatId";
+
+        internal Seat getSeat(int seatId)
+        {
+
+            DBConnection dbConnection = DBConnection.GetInstance();
+            SqlConnection connection = dbConnection.GetConnection();
+
+            Seat foundSeat = connection.Query<Seat>(_getSeatBySeatIdQuery, new { seatId = seatId }).First();
+
+            return foundSeat;
+        }
+    }
+}
