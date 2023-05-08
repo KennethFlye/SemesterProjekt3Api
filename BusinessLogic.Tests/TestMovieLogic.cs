645614e69455863300029e92006692216bd8bfce
MagnusPC <10434658@ucn.dk>
2023-05-08 11:29:23 +0200

    visibility fix and testmovielogic setup

---
+++ BusinessLogic.Tests/TestMovieLogic.cs
@@ -0,0 +1,101 @@
+﻿using SemesterProjekt3Api.BusinessLogic;
+using SemesterProjekt3Api.Model;
+using System.Diagnostics.Eventing.Reader;
+
+namespace BusinessLogic.Tests
+{
+    public class TestMovieLogic : IDisposable
+    {
+
+        private MovieLogic _movieLogic;
+
+        public TestMovieLogic()
+        {
+            _movieLogic = new MovieLogic();
+        }
+
+
+        [Fact]
+        public void TestGetMovieInfoList()
+        {
+            //Arrange
+            var result = _movieLogic.GetMovieInfoList();
+
+            //Act
+            if (result.Count > 0)
+            {
+                //Assert
+                Assert.NotEmpty(result);
+                Assert.IsType<MovieInfo>(result[0].GetType());
+            }
+            else if(result.Count < 1)
+            {
+                Assert.Empty(result);
+            }
+            else
+            {
+                Assert.Throws<InvalidOperationException>(() => result);
+            }
+        }
+
+        [Fact]
+        public void TestGetMovieCopyList()
+        {
+            //Arrange
+            var result = _movieLogic.GetMovieCopyList();
+
+            //Act
+            if (result.Count > 0)
+            {
+                //Assert
+                Assert.NotEmpty(result);
+                Assert.IsType<MovieCopy>(result[0].GetType());
+            }
+            else if (result.Count < 1)
+            {
+                //Assert
+                Assert.Empty(result);
+            }
+            else
+            {
+                //Assert
+                Assert.Throws<InvalidOperationException>(() => result);
+            }
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void TestGetShowingSListByMovieInfoId(int movieInfoId)
+        {
+            //Arrange
+            
+            //Act
+            var result = _movieLogic.GetShowingsByMovieInfoId(movieInfoId);
+
+            if (result.Count > 0)
+            {
+                //Assert
+                Assert.NotEmpty(result);
+                Assert.Equal(movieInfoId, result[0].MovieCopy.MovieType.infoId);
+            }
+            else if (result.Count < 1)
+            {
+                //Assert
+                Assert.Empty(result);
+            }
+            else
+            {
+                //Assert
+                Assert.Throws<InvalidOperationException>(() => result);
+            }
+        }
+
+        public void Dispose()
+        {
+            _movieLogic = null;
+        }
+
+    }
+}
