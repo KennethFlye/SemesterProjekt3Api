5e26a4a1cbb0138aee88ab4f56078b1c11fb5a9b
MagnusPC <10434658@ucn.dk>
2023-05-08 09:58:01 +0200

    seat lag opsat

--- SemesterProjekt3Api/BusinessLogic/SeatLogic.cs
+++ SemesterProjekt3Api/BusinessLogic/SeatLogic.cs
@@ -1,6 +1,24 @@
-﻿namespace SemesterProjekt3Api.BusinessLogic
+﻿using SemesterProjekt3Api.Database;
+using SemesterProjekt3Api.Model;
+
+namespace SemesterProjekt3Api.BusinessLogic
 {
     public class SeatLogic
     {
+        private DbSeat _dbSeat = new DbSeat();
+
+        internal Seat GetSeatBySeatId(int seatId)
+        {
+            Seat? foundSeat = null;
+            try
+            {
+                foundSeat = _dbSeat.getSeat(seatId);
+            }
+            catch (InvalidOperationException)
+            {
+                foundSeat = null;
+            }
+            return foundSeat;
+        }
     }
 }
