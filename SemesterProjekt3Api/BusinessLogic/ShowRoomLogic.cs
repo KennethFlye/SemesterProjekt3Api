8c5411aae5035953cd850f658b94065371b2001f
MagnusPC <10434658@ucn.dk>
2023-05-12 11:46:29 +0200

    delete comments in dbshowing, addition of showroom calls

---
+++ SemesterProjekt3Api/BusinessLogic/ShowRoomLogic.cs
@@ -0,0 +1,26 @@
+﻿using SemesterProjekt3Api.Database;
+using SemesterProjekt3Api.Model;
+
+namespace SemesterProjekt3Api.BusinessLogic
+{
+    public class ShowRoomLogic
+    {
+        private DbShowRoom _dbShowRoom = new DbShowRoom();
+
+        public List<ShowRoom>? GetShowRoomsList()
+        {
+            List<ShowRoom>? foundList = null;
+            
+            try
+            {
+                foundList = (List<ShowRoom>?)_dbShowRoom.GetAll();
+            }
+            catch (InvalidOperationException)
+            {
+                foundList = new List<ShowRoom>();
+            }
+
+            return foundList;
+        }
+    }
+}
---
+++ SemesterProjekt3Api/Controllers/ShowRoomController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SemesterProjekt3Api.BusinessLogic;
+using SemesterProjekt3Api.Model;
+
+namespace SemesterProjekt3Api.Controllers
+{
+    public class ShowRoomController : Controller
+    {
+        private ShowRoomLogic _showRoomLogic;
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            List<ShowRoom>? foundList = _showRoomLogic.GetShowRoomsList();
+
+            if (foundList.Count > 0)
+            {
+                return Ok(foundList);
+            }
+            else if (foundList.Count < 1)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+}
